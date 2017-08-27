635f31602103faad81da014e19cd279458c2059e
Shivam Goel <shvmgyl15@hotmail.com>
2017-08-28 04:41:43 +0530

    tested ok add, del, modify

--- app/src/main/java/com/bb/shivam/categoryplusone/RecyclerAdapter.java
+++ app/src/main/java/com/bb/shivam/categoryplusone/RecyclerAdapter.java
@@ -3,6 +3,7 @@ package com.bb.shivam.categoryplusone;
 import android.app.Dialog;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.support.design.widget.BaseTransientBottomBar;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -13,6 +14,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bb.shivam.categoryplusone.model.Category;
 
@@ -59,71 +61,120 @@ public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.Catego
             tV.append(category.getCategoryName());
         }
 
+//        void addOrModifyCategoryDialogBox(String dialogTitle, final String editText) {
+//
+//            final Dialog dialog = new Dialog(context);
+//            dialog.setContentView(R.layout.user_input_dialog_box);
+//
+//            final EditText userInputDialogEditText = (EditText) dialog.findViewById(R.id.userInputDialog);
+//            userInputDialogEditText.setText(editText);
+//
+//            TextView tVDialogTitle = (TextView) dialog.findViewById(R.id.tVDialog);
+//            tVDialogTitle.setText(dialogTitle);
+//
+//            Button dialogButtonSuccess = (Button) dialog.findViewById(R.id.btn_ok);
+//            Button dialogButtonCancel = (Button) dialog.findViewById(R.id.btn_cancel);
+//
+//            dialogButtonCancel.setOnClickListener(new View.OnClickListener() {
+//                @Override
+//                public void onClick(View v) {
+//                    dialog.dismiss();
+//                }
+//            });
+//            dialogButtonSuccess.setOnClickListener(new View.OnClickListener() {
+//                @Override
+//                public void onClick(View v) {
+//
+//                    String enteredText = userInputDialogEditText.getText().toString();
+//                    System.err.println(enteredText);
+//                    if(!enteredText.isEmpty()) {
+//                        if(editText == ""){
+//                            Category category = new Category(enteredText, mCategories.get(getAdapterPosition()).getCategoryName(), mCategories.get(getAdapterPosition()).getDistance() + 1);
+//                            addAt(category, getAdapterPosition());
+//                        }else{
+//                            mCategories.get(getAdapterPosition()).setCategoryName(enteredText);
+//                            bindCategory(mCategories.get(getAdapterPosition()));
+//                        }
+//                    }
+//                    dialog.dismiss();
+//                }
+//            });
+//
+//            dialog.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_VISIBLE);
+//            dialog.show();
+//
+//
+//        }
+
+
         void addOrModifyCategoryDialogBox(String dialogTitle, final String editText) {
 
-            final Dialog dialog = new Dialog(context);
-            dialog.setContentView(R.layout.user_input_dialog_box);
+            LayoutInflater layoutInflater = LayoutInflater.from(context);
+            View dialogView = layoutInflater.inflate(R.layout.user_input_dialog_box, new LinearLayout(context), false);
+            AlertDialog.Builder alertDialogBuilderUserInput = new AlertDialog.Builder(context);
+            alertDialogBuilderUserInput.setView(dialogView);
 
-            final EditText userInputDialogEditText = (EditText) dialog.findViewById(R.id.userInputDialog);
+            final EditText userInputDialogEditText = (EditText) dialogView.findViewById(R.id.userInputDialog);
             userInputDialogEditText.setText(editText);
-
-            TextView tVDialogTitle = (TextView) dialog.findViewById(R.id.tVDialog);
-            tVDialogTitle.setText(dialogTitle);
-
-            Button dialogButtonSuccess = (Button) dialog.findViewById(R.id.btn_ok);
-            Button dialogButtonCancel = (Button) dialog.findViewById(R.id.btn_cancel);
-
-            dialogButtonCancel.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    dialog.dismiss();
-                }
-            });
-            dialogButtonSuccess.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-
-                    String enteredText = userInputDialogEditText.getText().toString();
-                    System.err.println(enteredText);
-                    if(!enteredText.isEmpty()) {
-                        if(editText == ""){
-                            Category category = new Category(enteredText, mCategories.get(getAdapterPosition()).getCategoryName(), mCategories.get(getAdapterPosition()).getDistance() + 1);
-                            addAt(category, getAdapterPosition());
-                        }else{
-                            mCategories.get(getAdapterPosition()).setCategoryName(enteredText);
-                            bindCategory(mCategories.get(getAdapterPosition()));
+            alertDialogBuilderUserInput
+                    .setCancelable(false)
+                    .setTitle(dialogTitle)
+//                    .setMessage(dialogMessage)
+                    .setPositiveButton("SAVE", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            String enteredText = userInputDialogEditText.getText().toString();
+                            System.err.println(enteredText);
+                            if(!enteredText.isEmpty()) {
+                                if(editText == ""){
+                                    Category category = new Category(enteredText, mCategories.get(getAdapterPosition()).getCategoryName(), mCategories.get(getAdapterPosition()).getDistance() + 1);
+                                    addAt(category, getAdapterPosition() + 1);
+                                }else{
+                                    mCategories.get(getAdapterPosition()).setCategoryName(enteredText);
+                                    bindCategory(mCategories.get(getAdapterPosition()));
+                                }
+                            }
                         }
-                    }
-                    dialog.dismiss();
-                }
-            });
-
-            dialog.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_VISIBLE);
-            dialog.show();
-
-
+                    })
+                    .setNegativeButton("CANCEL", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            dialogInterface.cancel();
+                        }
+                    });
+            AlertDialog alertDialog = alertDialogBuilderUserInput.create();
+            alertDialog.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_VISIBLE);
+            alertDialog.show();
         }
+
 
         @Override
         public void onClick(View view) {
             int position = getAdapterPosition();
+
             switch (view.getId()){
                 case R.id.bAdd:
-                    if(mCategories.get(position).getDistance() == 0){
+                    if(position == 0){
                         addOrModifyCategoryDialogBox("ADD CATEGORY", "");
-                    }else{
-                        addOrModifyCategoryDialogBox("ADD SUB CATEGORY", "");
+                        return;
                     }
+                    addOrModifyCategoryDialogBox("ADD SUB CATEGORY", "");
                     break;
                 case R.id.bDelete:
+                    if(position == 0){
+                        Toast.makeText(context, "YOU CANNOT DELETE THE ROOT ELEMENT", Toast.LENGTH_SHORT);
+                        bDelete.setEnabled(false);
+                        return;
+                    }
                     removeAt(position);
                     break;
                 case R.id.bModify:
-                    if(mCategories.get(position).getDistance() == 0){
+                    if(position == 0){
+                        Toast.makeText(context, "YOU CANNOT MODIFY THE ROOT ELEMENT", Toast.LENGTH_SHORT);
                         bModify.setEnabled(false);
-                    }else{
-                        addOrModifyCategoryDialogBox("EDIT SUB CATEGORY NAME", mCategories.get(position).getCategoryName());
+                        return;
                     }
+                    addOrModifyCategoryDialogBox("EDIT SUB CATEGORY NAME", mCategories.get(position).getCategoryName());
                     break;
             }
         }
